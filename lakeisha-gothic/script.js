18a1e5988d2265a5216b1929e2758c6cd78ab6f2
lakeisha <sharl281@newschool.edu>
2024-10-16 00:27:37 -0400

    check

--- lakeisha-gothic/script.js
+++ lakeisha-gothic/script.js
@@ -1,6 +1,6 @@
 document.getElementById("toggleSwitch").addEventListener("change", function () {
   if (this.checked) {
-    window.location.href = "/lakeisha-gothic/index.html"; // Change to your target URL
+    window.location.href = "selects/lakeisha-gothic/index.html"; // Change to your target URL
   } else {
     window.location.href = "../index.html"; // Change to your default URL
   }
